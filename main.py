ed13ab6addc1dec5508b38bb918470bc6d219b4f
s0phia- <sophiajones@live.co.uk>
2022-12-01 11:06:41 +0100

    Add new agents

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tetris.game import Tetris
-from agents.ttb_sequential import TakeTheBestSequential
+from agents.ttb_q import Ttb_Q_values
 from agents.pure_ew import EqualWeights
+from agents.hand_crafted import HandCrafted
+from agents.ttb_validities import Ttb_validities
 from analysis.plot import gather_data, plot_gg, results_writer
 
 
@@ -11,36 +13,36 @@ if __name__ == "__main__":
     open_file, writer, run_id = results_writer(results_file)
 
     # Create a tetris env with directed features
-    env = Tetris(10, 10, False, feature_directions=[-1, -1, -1, -1, -1, -1, 1, -1])
+    env = Tetris(10, 10, feature_directions=[-1, -1, -1, -1, -1, -1, 1, -1])
     env.reset()
 
     # create an agent (may need some info from the environment)
     state = env.get_current_state_features()
-    agent = EqualWeights()  # agent = TakeTheBestSequential(env.num_features, state)
+    agent = Ttb_validities(env.num_features, state)
 
     # set up play loop
     done = False
     cleared_lines = 0
-    num_episodes = 1000
-    available_actions = env.get_after_states()
+    num_episodes = 100
+    available_actions, actions_inc_terminal = env.get_after_states(include_terminal=True)
     for x in range(num_episodes):
         while not done:
 
             # agent chooses an action, which is played
             action = agent.choose_action(available_actions)
-            print(available_actions[action])
             state_prime, reward, done, _ = env.step(action)
-            env.print_current_board()
+            # env.render()
 
             # env provides new actions for the agent to pick from, agent stores new knowledge and learns from it
-            available_actions_prime = env.get_after_states()
-            agent.store_data(state, state_prime, reward, available_actions_prime, done)
+            available_actions_prime, actions_inc_terminal_prime = env.get_after_states(include_terminal=True)
+            agent.store_data(action, available_actions_prime, reward)  # (state, state_prime, reward, available_actions_prime, done)
             agent.learn()
 
             # update vars for the next loop
             cleared_lines += reward
+            print(cleared_lines)
             state = state_prime
-            available_actions = available_actions_prime
+            available_actions, actions_inc_terminal = available_actions_prime, actions_inc_terminal_prime
 
         # game is over. Save stats and reset
         writer.writerow([x, cleared_lines, type(agent).__name__, run_id])
@@ -48,7 +50,7 @@ if __name__ == "__main__":
         cleared_lines = 0
         done = False
         state = env.get_current_state_features()
-        available_actions = env.get_after_states()
+        available_actions, actions_inc_terminal = env.get_after_states(include_terminal=True)
 
     # stop writing to the csv file, plot results
     open_file.close()
