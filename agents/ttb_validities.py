175d1a8696716b35522a85b23fee6e66717fbc5e
s0phia- <sophiajones@live.co.uk>
2022-12-19 18:31:27 +0100

    Bug fix, change == to <= in store_data

--- agents/ttb_validities.py
+++ agents/ttb_validities.py
@@ -3,17 +3,17 @@ from agents.ttb_sequential import TakeTheBestSequential
 from statistics import mean
 
 
-class Ttb_validities(TakeTheBestSequential):
+class TTB_validities(TakeTheBestSequential):
     def __init__(self, num_features, current_state):
         super().__init__(num_features, current_state)
 
     def store_data(self, action, available_actions, reward):
-        if reward == 0 or len(available_actions == 1):
+        if reward == 0 or len(available_actions) <= 1:
             return
+        print("something happened")
         selected_action = available_actions[action]
-        print(available_actions)
-        unselected_actions = available_actions[:action] + available_actions[action+1:]
-        feature_validity = np.zeros([1, self.num_features])
+        unselected_actions = np.delete(available_actions, action, axis=0)
+        feature_validity = np.zeros([self.num_features])
         for i in range(self.num_features):
             unselected_avg = mean([x[i] for x in unselected_actions])
             if selected_action[i] > unselected_avg:
