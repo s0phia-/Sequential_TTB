bf781330fb83c8be4529676e02b8cb2a5f24706f
s0phia- <sophiajones@live.co.uk>
2023-01-13 11:01:13 +0100

    Add rollouts agent

--- agents/ttb_rollouts.py
+++ agents/ttb_rollouts.py
@@ -1,19 +1,24 @@
 import numpy as np
-from ttb_sequential import TakeTheBestSequential
+from scipy.stats import pearsonr
+from agents.ttb_sequential import TakeTheBestSequential
 
 
 class TTB_Rollouts(TakeTheBestSequential):
     def __init__(self, num_features, current_state):
         super().__init__(num_features, current_state)
 
-    def store_data(self, action_rollout_returns: dict):
+    def store_data(self, rollout_actions: list, rollout_returns: list):
         """
-        remeber action selected and return after x time steps (where x is the length of a roll-out)
-        :param action_rollout_returns:
+        remember action selected and return after x time steps (where x is the length of a roll-out)
+        :param rollout_returns:
+        :param rollout_actions:
         :return:
         """
-        action_features = action_rollout_returns.items()
-        returns = action_rollout_returns.keys()
+        nonzero_return_ix = np.nonzero(rollout_returns)
+
+        action_features = np.array(rollout_actions)[nonzero_return_ix]
+        returns = np.array(rollout_returns)[nonzero_return_ix]
+
         self.xx = np.vstack([self.xx, action_features])
         self.yy = np.append(self.yy, returns)
 
@@ -23,8 +28,16 @@ class TTB_Rollouts(TakeTheBestSequential):
         This is a proxy for validity (in the multi-option case)
         No need to consider feature dependencies in this version of validity
         """
+        x = self.xx
+        y = self.yy
+        if len(y) < 2:  # must be length >=2 for correlation calc
+            return
+        # use sliding window
+        if len(y) > 1000:
+            x = x[-1000:, ]
+            y = y[-1000:]
         for i in range(self.num_features):
-            self.beta[i] = np.correlate(self.yy[:, i], self.xx)
+            self.beta[i] = pearsonr(y, x[:, i])[0]  # returns correlation ceof and p val, only want corr
 
     def feature_importance(self, state):
         feature_importance = np.argsort(np.argsort(self.beta))
