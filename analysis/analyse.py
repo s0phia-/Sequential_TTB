789af678c302441e198287683fce079a4ff8c473
s0phia- <sophiajones@live.co.uk>
2023-01-27 17:06:52 +0100

    Save plots, rather than just showing them

--- analysis/analyse.py
+++ analysis/analyse.py
@@ -1,8 +1,14 @@
 from analysis_methods import plot_gg, average_over_runs, get_data
 
-file_path = "../results/trial"
+file_path = "../results/hex_run_1"
 
 data = get_data(file_path)
 data = average_over_runs(data)
 
 plot_gg(data, "right")
+
+plot, fig = plot_gg(data, "bottom")
+fig.savefig('image1.png', dpi=300, bbox_inches='tight')
+
+plot, fig = plot_gg(data, "none")
+fig.savefig('image2.png', dpi=300, bbox_inches='tight')
