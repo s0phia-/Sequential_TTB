1b5bd0804d0f3657d446163e1d55200f4e388b39
s0phia- <sophiajones@live.co.uk>
2023-03-24 12:49:09 +0100

    Add new agents to import

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from agents.ttb_rollout_validities import TtbRollVal, TtbRRollCondVal
 from agents.RandomTTB import RandomTTB
 from agents.ttb_rollouts import TtbRolloutsCorrelation, TtbRolloutsSummingScore
 from agents.ttb_sequential import TTBFixedWeights
+from agents.ttb_exploit_q import ExploitV
 from agents.state_dependent_ttb import StateDependent
 from run_files import run_ttb_val, run_ttb_q, run_ttb_rollouts, run_simple, pool_run, run_ttb_q_seq
 
@@ -22,15 +23,15 @@ if __name__ == "__main__":
         os.makedirs(results_file)
 
     # create multiprocessing pools
-    pool = mp.Pool(mp.cpu_count())
+    pool = mp.Pool(1)  # mp.cpu_count())
 
     # parameters
-    agents = [[StateDependent, run_ttb_q_seq]]  # agents to try
+    agents = [[StateDependent, run_simple]]  # agents to try
 
     number_of_agents = 1  # number of agents to average performance over
-    num_episodes = 10**5
-    rows = 10
-    cols = 10
+    num_episodes = 10**2
+    rows = 8
+    cols = 8
 
     all_run_args = [[agent_name, agent_i, results_file, num_episodes, rows, cols, play_loop]
                     for agent_name, play_loop in agents
