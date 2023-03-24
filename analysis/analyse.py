b7a7dee804239faf8d28c4bb515479539e4fe47e
s0phia- <sophiajones@live.co.uk>
2023-03-24 12:47:04 +0100

    Allow for more input data formats

--- analysis/analyse.py
+++ analysis/analyse.py
@@ -1,15 +1,26 @@
 from analysis_methods import plot_gg, tidy_data_episodes, get_data, tidy_data_steps, plot_scatter
 
-file_path = "../results/dunno"
+# file_path = "../results/smalltet"
+# data = get_data(file_path)
 
-data = get_data(file_path)
+import pandas as pd
+csv_data = pd.read_csv('../results/ttb/ExploitV_0.csv')
+data = pd.DataFrame({'agent': 'v' * len(csv_data),
+                     'agent_number': '0' * len(csv_data),
+                     'step': csv_data['step'],
+                     'episode': csv_data['episode_number'],
+                     'return': csv_data['return']})
+print("done stage 1")
+
 ep_data = tidy_data_episodes(data)
+
+print("done stage 2")
 
 plot_gg(ep_data, "right")
 
-step_data = tidy_data_steps(data)
-
-plot_scatter(step_data, "right")
+# step_data = tidy_data_steps(data)
+#
+# plot_scatter(step_data, "right")
 
 # plot, fig = plot_gg(data, "bottom")
 # fig.savefig('image1.png', dpi=300, bbox_inches='tight')
